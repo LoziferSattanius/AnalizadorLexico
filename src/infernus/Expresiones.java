487949b0a793c7b67183e994f7bce278ef4b6d48
LoziferSattanius <jorge-lozornio-vazquez@hotmail.com>
2020-03-10 07:41:24 -0600

    Creo que ya se subio

--- src/infernus/Expresiones.java
+++ src/infernus/Expresiones.java
@@ -1,7 +1,125 @@
 
 package infernus;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Expresiones {
+    
+    String palabra= new String();
+    
+    public String getToken(String token){        
+        switch(token){
+            case "Origin":
+                palabra = "Inicio de programa";
+                break;
+            case "death":
+                palabra = "Fin de programa";
+                break;
+            case "int":
+                palabra = "Variable tipo entero";
+                break;
+            case "fly":
+                palabra = "variable tipo flotante";
+                break;
+            case "bool":
+                palabra = "variable tipo boleano";
+                break; 
+            case "txt":
+                palabra = "variable tipo texto";
+                break; 
+            case "asc":
+                palabra = "variable tipo char";
+                break;
+            case "shoot":
+                palabra = "Impresora";
+                break;  
+            case "read":
+                palabra = "Lector de variables";
+                break;
+            case "kill":
+                palabra = "Instruccion break";
+                break; 
+            case "true":
+                palabra = "Verdadero";
+                break;
+            case "Falso":
+                palabra = "Falso";
+                break;  
+            case "wild":
+                palabra = "Ciclo while";
+                break;  
+            case "for":
+                palabra = "Ciclo for";
+                break;  
+            case "if":
+                palabra = "Desicion if";
+                break;  
+            case "else":
+                palabra = "Sino";
+                break; 
+            case "<<":
+                palabra = "Llave de control de inicio";
+                break;  
+            case ">>":
+                palabra = "Llave de control final";
+                break;  
+            case "(":
+                palabra = "Inicio parentesis";
+                break; 
+            case ")":
+                palabra = "Fin parentesis";
+                break;
+            case "==":
+                palabra = "Comparacion igual";
+                break;
+            case "+":
+                palabra = "Signo aritmetico de suma";
+                break;
+            case "-":
+                palabra = "Signo aritmetico de menos";
+                break;
+            case "*":
+                palabra = "Signo aritmetico de multiplicacion";
+                break;
+            case "/":
+                palabra = "Signo aritmetico de division";
+                break;
+            case ";":
+                palabra = "Termino de linea";
+                break;
+            case "=":
+                palabra = "Signo de igual";
+                break;
+            case "<":
+                palabra = "Signo menor que";
+                break;
+            case ">":
+                palabra = "Signo mayor que";
+                break;
+            case "<>":
+                palabra = "Signo diferente que";
+                break;
+            case "&":
+                palabra = "Signo Si";
+                break;
+            case "|":
+                palabra = "Signo O";
+                break;                
+            default:
+                
+                if(token.matches("[0-9]+[.][0-9]+")){
+                    return palabra = "Valor flotante";
+                }
+                if(token.matches("[0-9]+")){
+                        return palabra = "Valor entero";                   
+                }
+                if(token.matches("[A-Za-z]+[0-9]*[A-Za-z]*")){//000009
+                    return palabra = "Variable";
+                }
+        }
+        return palabra;
+    }
     
 }
--- src/infernus/Infernus.java
+++ src/infernus/Infernus.java
@@ -1,6 +1,8 @@
 
 package infernus;
 
+import Frames.AnalizadorLexico;
+
 
 public class Infernus {
 
@@ -8,6 +10,9 @@ public class Infernus {
     public static void main(String[] args) {
         
         System.out.println("Hola mundo");
+        System.out.println("Perro");
+        AnalizadorLexico al =new AnalizadorLexico();
+        al.setVisible(true);
         
     }
     
